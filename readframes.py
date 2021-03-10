6cc7edd2167e43d0cb92692253cbddfb05e50c15
Rech99 <rech_99@hotmail.com>
2021-03-10 11:03:30 -0700

    int-np

--- readframes.py
+++ readframes.py
@@ -1,4 +1,5 @@
 import wave
+import numpy as np
 
 #cargar archivo wav en la variable
 
@@ -9,3 +10,9 @@ frames = goodmorning.readframes(-1)
 
 #mostrar el resultado de frames
 print(frames[:10])
+
+#Convierte el audio good morning de bytes a enteros
+ondaconvertida = np.frombuffer (frames, dtype='int16')
+
+#Muestra los primeros 10 int
+print (ondaconvertida [:10])
